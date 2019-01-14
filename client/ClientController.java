07aa75eb3864d5bdd05da13b6b12f56bf64a11e3
KonstantinFed12344 <32627734+KonstantinFed12344@users.noreply.github.com>
2019-01-14 00:42:56 -0500

    Add files via upload

---
+++ client/ClientController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import java.net.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+/**
+ *
+ * @author Konstantin
+ */
+public class ClientController implements Initializable {
+    
+    @FXML
+    private TextField host;
+    @FXML
+    private TextField port;
+    @FXML
+    private Label error;
+    
+    private Socket socket;
+    
+    public void connect() throws InterruptedException {
+        try {
+            socket = new Socket(InetAddress.getByName(host.getText()),Integer.parseInt(port.getText()));
+        } catch (Exception e) {
+            error.setOpacity(1);
+            Thread.sleep(1000);
+            error.setOpacity(0);
+            
+        }
+    }
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
