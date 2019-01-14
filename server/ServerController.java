0b18fb25bad8337a3dd6dcea1378ae49d683a23c
KonstantinFed12344 <32627734+KonstantinFed12344@users.noreply.github.com>
2019-01-14 00:49:11 -0500

    Add files via upload

---
+++ server/ServerController.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.net.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+/**
+ *
+ * @author Konstantin
+ */
+public class ServerController implements Initializable {
+
+    @FXML
+    private TextField host;
+    @FXML
+    private TextField port;
+    private TextField client;
+    @FXML
+    private Button connectButton;
+    @FXML
+    private Label error;
+
+    private ServerSocket server;
+    private Socket clientSocket;
+
+    @FXML
+    public void connect() throws InterruptedException {
+        try {
+            clientSocket = server.accept();
+            client.setText(clientSocket.getInetAddress().getHostAddress());
+        } catch (Exception e) {
+            System.out.println("Server Error");
+            error.setOpacity(1);
+            Thread.sleep(1000);
+            error.setOpacity(0);
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+        try {
+            server = new ServerSocket(0, 1, InetAddress.getLocalHost());
+            host.setText(server.getInetAddress().getHostAddress());
+            port.setText(Integer.toString(server.getLocalPort()));
+            
+        } catch (Exception e) {
+            System.out.println("Error");
+            
+
+        }
+
+    }
+
+}
