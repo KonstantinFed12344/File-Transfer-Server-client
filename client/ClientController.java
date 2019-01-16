5ebc31703eaf341fd9ad0bc9655abe71c2be608b
KonstantinFed12344 <32627734+KonstantinFed12344@users.noreply.github.com>
2019-01-15 22:29:47 -0500

    Add files via upload

--- client/ClientController.java
+++ client/ClientController.java
@@ -5,10 +5,15 @@
  */
 package client;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.*;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -28,12 +33,32 @@ public class ClientController implements Initializable {
     private TextField port;
     @FXML
     private Label error;
+    @FXML
+    private ListView<String> fileList;
+    @FXML
+    private Button folder;
 
     private Socket socket;
+    private ArrayList<String> files;
+
+    @FXML
+    private void openFolder() throws IOException {
+        Runtime.getRuntime().exec("explorer.exe /open, C:\\FileBankReceived");
+    }
 
     public void connect() throws InterruptedException {
         try {
+
             socket = new Socket(InetAddress.getByName(host.getText()), Integer.parseInt(port.getText()));
+            String fileNames = null;
+            BufferedReader serverFiles = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            files = new ArrayList<>();
+
+            while ((fileNames = serverFiles.readLine()) != null) {
+                files.add(fileNames);
+                //System.out.println(files.get(files.size()-1));
+                fileList.getItems().add(fileNames);
+            }
         } catch (Exception e) {
             System.out.println("Client Error");
             error.setOpacity(1);
