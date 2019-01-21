2c350e016287b728bad1d4e146c4ac5b10b54282
KonstantinFed12344 <32627734+KonstantinFed12344@users.noreply.github.com>
2019-01-21 02:13:38 -0500

    Add files via upload

--- server/ServerQueue.java
+++ server/ServerQueue.java
@@ -5,9 +5,7 @@
  */
 package server;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
 import javafx.concurrent.Task;
@@ -30,6 +28,9 @@ public class ServerQueue extends Task {
     protected String call() throws Exception {
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
         BufferedReader clientFile = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        DataOutputStream fileOut = new DataOutputStream(new BufferedOutputStream(clientSocket.getOutputStream()));
+        DataInputStream fileIn = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+        BufferedInputStream br;
         for (String files : files) {
             System.out.println(files);
             out.println(files);
@@ -37,9 +38,37 @@ public class ServerQueue extends Task {
         }
 
         String fileName;
+        File file;
+        byte[] bytes;
+        long length;
         while (true) {
             fileName = clientFile.readLine();
             System.out.println(fileName);
+            file = new File("C:/FileBank/" + fileName);
+//            
+//            bytes = new byte[(int)file.length()];
+//            out.println(bytes.length);
+//            br = new BufferedInputStream(new FileInputStream(file));
+//            int b;
+//            int currentBytes = 0;
+//            
+//            while ((b = br.read()) != -1 ){
+//               bytes[currentBytes++] = (byte)b;                
+//            }
+//            
+//            System.out.println(bytes.length);
+//            
+//            for(b=0;b<bytes.length;b++){
+//                clientSocket.getOutputStream().write(bytes[b]);
+//            }
+
+//            length = file.length();
+//            if (length <= Long.MAX_VALUE) 
+//                
+//                bytes = new byte[(int) length];
+//                fileOut.write(bytes, 0, (int)length);
+//            }
+
         }
 
     }
